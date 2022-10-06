807c29745bdb804a0bb32a9d898bf55965ed65a0
Johnny <johnny.gomes@ufjf.br>
2022-10-06 11:04:10 -0300

    Quebra em componentes e dados dinâmicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { LinhaProdutosComponent } from './linha-produtos/linha-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    FooterComponent,
+    LinhaProdutosComponent
   ],
   imports: [
     BrowserModule
